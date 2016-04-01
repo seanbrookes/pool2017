596df80c287864079faa32c234a1fec4f531168e
seanbrookes <seanbrookes@beachair.ca>
2016-04-01 01:20:29 -0700

    More CSS tweaks
    - set draft as default home screeen for now

--- client/www/scripts/modules/main/main.module.js
+++ client/www/scripts/modules/main/main.module.js
@@ -29,8 +29,10 @@ Main.config([
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: './scripts/modules/main/templates/main.html',
-        controller: 'MainController'
+        //templateUrl: './scripts/modules/main/templates/main.html',
+        //controller: 'MainController'
+        controller:'DraftMainController',
+        templateUrl:'./scripts/modules/draft/templates/draft.main.html'
       })
       .state('roster', {
         url:'/roster/:slug',
